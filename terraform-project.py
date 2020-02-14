247919f3e90f48954afa12b750fc48e4eff35e30
Dev .Python <lbrealdeveloper@gmail.com>
2020-02-14 23:35:23 +0100

    Update develop

--- terraform-project.py
+++ terraform-project.py
@@ -6,8 +6,6 @@ MODULES = "modules"
 HOMEPATH = str(Path.home())
 ENTRYPOINT = "\\Documents\\"
 
-
-
 name_dir_project = str(input("Nome do seu projeto: "))
 git = str(input("git init (Y/N): "))
 
@@ -26,44 +24,44 @@ def tree_project():
     else:
         os.chdir(a)
     if not os.path.exists(dir_project):
-        os.mkdir(name_dir_project)   # Cria diretório do projeto
-        os.chdir(name_dir_project)   # Dentro da pasta criada
-        os.mkdir(MODULES)       # Cria dir modules
+        os.mkdir(name_dir_project)
+        os.chdir(name_dir_project)
+        os.mkdir(MODULES)
         os.chdir(MODULES)
-        for folder in folders:  # Cria diretórios dev,uat,prod
+        for folder in folders:
             os.mkdir(folder) 
-        os.chdir(folders[0])    # Dentro de dev
-        for sub1 in sub_folders: # Cria diretórios back,compute,network
+        os.chdir(folders[0])
+        for sub1 in sub_folders:
             os.mkdir(sub1)
         if os.path.isdir(sub_folders[0]):
-            os.chdir(sub_folders[0]) # Files in backend
+            os.chdir(sub_folders[0])
             for tfsback in tfs:
-                f = open(f'{tfsback}.tf', 'w')
+                open(f'{tfsback}.tf', 'w')
             os.chdir('..')
-            os.chdir(sub_folders[1]) # Files in compute
+            os.chdir(sub_folders[1])
             for tfscompute in tfs:
-                f = open(f'{tfscompute}.tf', 'w')
+                open(f'{tfscompute}.tf', 'w')
             os.chdir('..')
-            os.chdir(sub_folders[2]) # files in network
+            os.chdir(sub_folders[2])
             for tfsnetwork in tfs:
-                f = open(f'{tfsnetwork}.tf', 'w')
+                open(f'{tfsnetwork}.tf', 'w')
             os.chdir('..')
         os.chdir('..')
-        os.chdir(folders[1]) # Dentro de uat
+        os.chdir(folders[1])
         for sub2 in sub_folders:
             os.mkdir(sub2)
         if os.path.isdir(sub_folders[0]):
             os.chdir(sub_folders[0])
             for tfsback2 in tfs:
-                f = open(f'{tfsback2}.tf', 'w')
+                open(f'{tfsback2}.tf', 'w')
             os.chdir('..')
             os.chdir(sub_folders[1])
             for tfscompute2 in tfs:
-                f = open(f'{tfscompute2}.tf', 'w')
+                open(f'{tfscompute2}.tf', 'w')
             os.chdir('..')
             os.chdir(sub_folders[2])
             for tfsnetwork2 in tfs:
-                f = open(f'{tfsnetwork2}.tf', 'w')
+                open(f'{tfsnetwork2}.tf', 'w')
             os.chdir('..')
         os.chdir('..')
         os.chdir(folders[2])
@@ -72,20 +70,20 @@ def tree_project():
         if os.path.isdir(sub_folders[0]):
             os.chdir(sub_folders[0])
             for tfsback3 in tfs:
-                f = open(f'{tfsback3}.tf', 'w')
+                open(f'{tfsback3}.tf', 'w')
             os.chdir('..')
             os.chdir(sub_folders[1])
             for tfscompute3 in tfs:
-                f = open(f'{tfscompute3}.tf', 'w')
+                open(f'{tfscompute3}.tf', 'w')
             os.chdir('..')
             os.chdir(sub_folders[2])
             for tfsnetwork3 in tfs:
-                f = open(f'{tfsnetwork3}.tf', 'w')
+                open(f'{tfsnetwork3}.tf', 'w')
             os.chdir('..')
             os.chdir('..')
         os.chdir('..')
         for mainfiles in mainfilesdir:
-            f = open(f'{mainfiles}', 'w')
+            open(f'{mainfiles}', 'w')
         if git == "y":
             os.system('git init')
             os.system('cls')
